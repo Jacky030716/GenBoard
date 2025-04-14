87c2c37ceb051975c4cd319fbd29d23b0f924cf5
Jacky030716 <huan030716@gmail.com>
2025-04-14 21:22:37 +0800

    Add create meeting function

--- client/src/pages/trainer/TrainerMeeting.tsx
+++ client/src/pages/trainer/TrainerMeeting.tsx
@@ -1,5 +1,10 @@
-import { MeetingScheduler } from "@/features/trainer/components/MeetingScheduler";
+import {
+  MeetingFormValues,
+  MeetingScheduler,
+} from "@/features/trainer/components/MeetingScheduler";
 import { Header } from "@/features/trainer/components/Header";
+import { useCreateMeeting } from "@/features/trainer/hooks/use-create-meeting";
+import { format } from "date-fns";
 
 const exampleTrainees = [
   { id: "1", name: "John Doe" },
@@ -9,10 +14,30 @@ const exampleTrainees = [
 ];
 
 const TrainerMeeting = () => {
+  const createMeeting = useCreateMeeting();
+
+  const handleSubmit = async (data: MeetingFormValues) => {
+    createMeeting.mutate(
+      {
+        ...data,
+        date: new Date(data.date),
+        host: "1234567",
+      },
+      {
+        onSuccess: () => {
+          console.log("Meeting created successfully");
+        },
+        onError: (error) => {
+          console.error("Error creating meeting:", error);
+        },
+      }
+    );
+  };
+
   return (
     <div className="flex-1 flex flex-col gap-12 w-full bg-background-light p-8">
       <Header title="Arrange a meeeting" />
-      <MeetingScheduler onSubmit={() => {}} trainees={exampleTrainees} />
+      <MeetingScheduler onSubmit={handleSubmit} trainees={exampleTrainees} />
     </div>
   );
 };
--- server/services/meeting_service.js
+++ server/services/meeting_service.js
@@ -1,18 +1,18 @@
 const Meeting = require("../models/meeting_model");
 
 const meetingService = {
-    createMeeting: async (data) => {
-        const newMeeting = new Meeting(data);
-        return await newMeeting.save();
-    },
+  createMeeting: async (data) => {
+    const newMeeting = new Meeting(data);
+    return await newMeeting.save();
+  },
 
-    getMeetingsByHost: async (uid) => {
-        return await Meeting.find({ host: uid });
-    },
+  getMeetingsByHost: async (uid) => {
+    return await Meeting.find({ host: uid });
+  },
 
-    getMeetingsByParticipant: async (uid) => {
-        return await Meeting.find({ participants: uid });
-    }
+  getMeetingsByParticipant: async (uid) => {
+    return await Meeting.find({ participants: uid });
+  },
 };
 
 module.exports = meetingService;
